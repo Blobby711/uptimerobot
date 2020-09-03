42522367de3deb907af17378bf1a5649236b977a
Blobby711 <68063650+Blobby711@users.noreply.github.com>
2020-09-03 17:13:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://verbose-incongruous-clementine.glitch.me"]
+const urls = ["https://the-bongo.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
